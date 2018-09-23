c4c7596c9d77a9cf60dc930ab5d0f91fcb4f89c1
mymymy1303 <thehuy160897@gmail.com>
2018-09-23 21:57:24 +0700

    Add comments to source code

--- src/scripts/controllers/users.controller.js
+++ src/scripts/controllers/users.controller.js
@@ -4,16 +4,28 @@ angular.module('users.controller', []).controller("UsersController", _usersContr
 
 function _usersController($scope, $http, $filter, UsersService) {
 	$scope.title = 'List of users'
+
+	//Current displaying page. Bind to enter page number textbox
 	$scope.currentPage = 1
+
+	//Max numbers displaying on the center of the range between the first page and last page.
 	$scope.maxSize = 3
+
+	//Bind with search textbox, displaying any result matches the entered value.
 	$scope.searchInput = ""
-	$scope.pagesNum = 0
+
+	//Options for selecting number of items display on page.
 	$scope.pageSizeArray = [5, 10, 15, 20, 25]
+
+	//Bind with items per page listbox.
 	$scope.pageSize = $scope.pageSizeArray[1]
 
+	//Send get request to get all users listed in database.
 	UsersService.get($http)
 		.then(response => {
 			$scope.data = response.data
+
+			//Calculated the number of pages.
 			let temp = $scope.data.length / $scope.pageSize
 			$scope.numPages = (temp === parseInt(temp)) ? temp : Math.floor(temp) + 1
 		})
--- src/scripts/services/users.service.js
+++ src/scripts/services/users.service.js
@@ -1,5 +1,7 @@
 angular.module('users.service', []).service('UsersService', _usersServices)
 
+//Send GET request, no parameter, to WebAPI server to get all users listed in database.
+//Return a Promise, need to be handled wherever it's call.
 function _usersServices() {
 	let baseUrl = 'http://localhost:5050/users'
 
